fcaa367600e05a5b6c8ecbd0693a8ea0cba86750
pbs2h16ala <stephanl.1995@web.de>
2018-07-19 22:53:58 +0200

    Added Dealer Mod

---
+++ copsV/copsV/Content/Economy/Dealer/Backpack.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace copsV.Content.Economy.Dealer
+{
+    class Backpack
+    {
+        private int size;
+        private Item[] items;
+
+        public Backpack()
+        {
+
+        }
+
+        public int Size { get => size; set => size = value; }
+        internal Item[] Items { get => items; set => items = value; }
+
+        public void Setup()
+        {
+            this.size = 25;
+            this.items = new Item[Size];
+        }
+
+        public void Setup(int size)
+        {
+            this.size = size;
+            this.items = new Item[Size];
+        }
+
+        public bool AddItem(Item item)
+        {
+            List<int> freeItemSlots = this.GetFreeItemSlots();
+
+            if (freeItemSlots.Count > 0)
+            {
+                int freeSlot = freeItemSlots.ElementAt<int>(0);
+
+                this.items[freeSlot] = item;
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public bool AddItemAtIndex(Item item, int index)
+        {
+            if (this.IsItemSlotEmpty(index))
+            {
+                this.items[index] = item;
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public void RemoveItem(int index)
+        {
+            this.items[index] = new Item();
+            this.items[index].SetupEmptyItem();
+        }
+
+        public List<int> GetFreeItemSlots()
+        {
+            List<int> freeItemSlots = new List<int>();
+
+            for (int i = 0; i < this.items.Length; i++)
+            {
+                if (this.IsItemSlotEmpty(i))
+                {
+                    freeItemSlots.Add(i);
+                }
+            }
+
+            return freeItemSlots;
+        }
+
+        public bool IsItemSlotEmpty(int index)
+        {
+            if (this.items[index].Name == "empty")
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ copsV/copsV/Content/Economy/Dealer/Player.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace copsV.Content.Economy.Dealer
+{
+    class Player
+    {
+
+        public enum DealerRank:int { StreetWalker=0, Runner=1, Allocator=2, Wannabe=3, Prooved=4, Respected=5, Influencer=6, RightHand=7, Leader=8, Owner=9, Legend=10  }
+        public enum CustomerReputation:int { None=0, Best=1, Great=2, Liked=3, Statisfied=4, Unlucky=5, Hatefull=6, Endangered = 7 }
+
+
+        private double currentMoney;
+        private int rankProgress;
+        private int reputationProgress;
+
+        private Backpack backpack;
+    }
+}
